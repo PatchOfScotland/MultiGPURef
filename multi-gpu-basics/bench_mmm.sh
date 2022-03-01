07fcae506dfc524b1777d24c66c9b1b17be62bbb
demiguard <demiguard@gmail.com>
2022-03-01 11:27:30 +0100

    Added more time to the script

--- multi-gpu-basics/bench_mmm.sh
+++ multi-gpu-basics/bench_mmm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=MultiGPUMap
 #SBATCH --ntasks=7
-#SBATCH --time=00:20:00
+#SBATCH --time=01:20:00
 #SBATCH --mem=10000m
 #SBATCH -p gpu --gres=gpu:gtx1080:3
 
-nvcc src/mmm_bench_multi.cu -o build/mmm_bench_multi -std=c++11 -O3
-nvcc src/mmm_bench_single.cu -o build/mmm_bench_single -std=c++11 -O3
+nvcc src/mmm_bench_multi.cu -o build/mmm_bench_multi -O3
+nvcc src/mmm_bench_single.cu -o build/mmm_bench_single -O3
 nvcc src/mmm_bench_multi_prefetch.cu -o build/mmm_bench_prefetch -O3
 
 ./build/mmm_bench_multi data/mmm_bench_multi_3.csv
