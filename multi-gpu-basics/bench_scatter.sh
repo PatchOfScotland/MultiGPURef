50050215e36907ef13d3cb6cee2ad5518aae9739
demiguard <demiguard@gmail.com>
2022-03-08 14:13:37 +0100

    fixed inputbug

--- multi-gpu-basics/bench_scatter.sh
+++ multi-gpu-basics/bench_scatter.sh
@@ -12,7 +12,7 @@ nvcc src/scatter_bench_multi_no_reset.cu -o build/scatter_bench_multi_no_reset -
 
 ./build/scatter_bench_single data/scatter_bench_single.csv
 ./build/scatter_bench_multi data/scatter_bench_multi.csv
-./build/scatter_bench_single_no_reset data/build/scatter_bench_single_no_reset.csv
-./build/scatter_bench_multi_no_reset data/build/scatter_bench_multi_no_reset.csv
+./build/scatter_bench_single_no_reset data/scatter_bench_single_no_reset.csv
+./build/scatter_bench_multi_no_reset data/scatter_bench_multi_no_reset.csv
 
 echo "DONE!"
