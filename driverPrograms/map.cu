b95d25d37fb1dfdb1d454b8fe054698b64bdf5d4
demiguard <demiguard@gmail.com>
2022-03-29 18:43:52 +0200

    Added device allocation to calls

--- driverPrograms/map.cu
+++ driverPrograms/map.cu
@@ -65,8 +65,7 @@ int main(int argc, char** argv){
 
     for(int devID = 0; devID < DeviceCount; devID++){
         CUDA_SAFE_CALL(cuDeviceGet(&devices[devID], devID));
-
-        CUDA_SAFE_CALL(cuCtxCreate(&contexts[devID], CU_CTX_SCHED_AUTO, devices[devID]));
+        CUDA_SAFE_CALL(cuCtxCreate(&contexts[devID], CU_CTX_SCHED_AUTO, devices[devID])); // This Automaticly set the device
         CUDA_SAFE_CALL(cuStreamCreate(&streams[devID], CU_STREAM_DEFAULT));
         CUDA_SAFE_CALL(cuEventCreate(&BenchmarkEvents[devID*2] ,CU_EVENT_DEFAULT)); // Start Event
         CUDA_SAFE_CALL(cuEventCreate(&BenchmarkEvents[devID*2 + 1] ,CU_EVENT_DEFAULT)); // Stop Event
@@ -98,7 +97,7 @@ int main(int argc, char** argv){
     int* destData = (int*)malloc(N*sizeof(int));    
 
     #if UNIFIED
-    int* hostData = (int*)mem_in;
+    int* hostData = (int*)mem_in; //You can do this, which looks horrible
     for(int i = 0; i < N; i++){
         hostData[i] = i;
     }
@@ -113,6 +112,7 @@ int main(int argc, char** argv){
 
     #if UNIFIED
     for(int devID = 0; devID < DeviceCount; devID++){
+        CUDA_SAFE_CALL(cuCtxSetCurrent(contexts[devID]));
         const size_t ElemsPerDevice = BlocksPerDevice*BlockSize;
         const size_t offset = ElemsPerDevice * devID;
         const size_t ElementsToPrefetch = (offset + ElemsPerDevice < N) ? ElemsPerDevice : N - offset;
@@ -128,6 +128,7 @@ int main(int argc, char** argv){
     for(int run = 0; run < GPU_RUNS; run++){
         
     for(int devID = 0; devID < DeviceCount; devID++){
+            CUDA_SAFE_CALL(cuCtxSetCurrent(contexts[devID]));
             void *args[] = {&mem_in, &mem_out, &N, &devID};
             CUDA_SAFE_CALL(cuEventRecord(BenchmarkEvents[devID*2], streams[devID]));
             CUDA_SAFE_CALL(cuLaunchKernel(Kernels[0], 
@@ -140,6 +141,7 @@ int main(int argc, char** argv){
         }
         float runTimes[DeviceCount];
         for(int devID = 0; devID < DeviceCount; devID++){
+            CUDA_SAFE_CALL(cuCtxSetCurrent(contexts[devID]));
             CUDA_SAFE_CALL(cuStreamSynchronize(streams[devID]));
             CUDA_SAFE_CALL(cuEventElapsedTime(&runTimes[devID], BenchmarkEvents[devID*2], BenchmarkEvents[devID*2 + 1]));
             printf("%f\n", runTimes[devID]);
@@ -159,10 +161,11 @@ int main(int argc, char** argv){
     cuModuleUnload(module);
 
     for(int devID = 0; devID < DeviceCount; devID++){
-        cuEventDestroy(BenchmarkEvents[devID*2]);
-        cuEventDestroy(BenchmarkEvents[devID*2 + 1]);
-        cuStreamDestroy(streams[devID]); //Destroy Streams first
-        cuCtxDestroy(contexts[devID]);
+        CUDA_SAFE_CALL(cuCtxSetCurrent(contexts[devID]));
+        CUDA_SAFE_CALL(cuEventDestroy(BenchmarkEvents[devID*2]));
+        CUDA_SAFE_CALL(cuEventDestroy(BenchmarkEvents[devID*2 + 1]));
+        CUDA_SAFE_CALL(cuStreamDestroy(streams[devID])); //Destroy Streams first
+        CUDA_SAFE_CALL(cuCtxDestroy(contexts[devID]));
     }
 
 
