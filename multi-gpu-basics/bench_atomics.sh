a4e59b1fded8177770b9b9a3c93d6898803a289e
demiguard <demiguard@gmail.com>
2022-05-28 21:26:06 +0200

    forgot this script

---
+++ multi-gpu-basics/bench_atomics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=MultiGPUMap
+#SBATCH --ntasks=3
+#SBATCH --time=00:10:00
+#SBATCH --mem=20000m
+#SBATCH -p gpu --gres=gpu:gtx1080:3
+
+nvcc src/atomic_bench.cu -o build/atomic_bench -O3 -std=c++11 -arch=sm_61
+
+./build/atomic_bench data/atomic_results.csv
+
+echo "DONE!"
