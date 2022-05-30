3ca8bf6a50faecfbf1330fd26fc13ac768a3442b
demiguard <demiguard@gmail.com>
2022-05-30 10:41:34 +0200

    Fix

--- multi-gpu-basics/src/lib/jacobi_relaxsation.cu
+++ multi-gpu-basics/src/lib/jacobi_relaxsation.cu
@@ -167,7 +167,7 @@ namespace singleGPU {
         const dim3 block(blockSize, blockSize, 1);
 
         while(norm > TOL && iter < MAX_ITER){
-            CUDA_RT_CALL(cudaMemset(*norm_d, 0, sizeof(float)));
+            CUDA_RT_CALL(cudaMemset(norm_d[0], 0, sizeof(float)));
             dim3 grid(colBlocks, rowBlocks, 1);
 
             jacobiKernel<blockSize><<<grid, block, shmemSize>>>(
