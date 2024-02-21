17cc568cf7db2d00bb72de6c05339ae74e30638e
PatchOfScotland <d.marchant@ed-alumni.net>
2024-02-21 15:27:30 +0100

    added shared header file

--- patch/src/map.cu
+++ patch/src/map.cu
@@ -1,16 +1,52 @@
-#include <iostream>
+#include "shared.cu.h"
 
-typedef float funcType;
+float PlusConst(const float inputElement, const float x) {
+    return inputElement + x;
+}
 
 int main(int argc, char** argv){
-    if (argc != 2)
+    if (argc < 2)
     {
-        std::cout << "Usage: " << argv[0] << " <array length>\n";
+        std::cout << "Usage: " 
+                  << argv[0] 
+                  << " <array length> -v(optional)\n";
         exit(EXIT_FAILURE);
     } 
 
     unsigned int array_len = atoi(argv[1]);
+    bool validating = false;
 
-    std::cout << "Running array of length " << array_len << " (" << ((array_len*2*sizeof(funcType))/1e9) <<"GB)\n";
+    for (int i=0; i<argc; i++) {
+        if (strcmp(argv[i], "-v") == 0) {
+            validating = true;
+        }
+    }
 
+    std::cout << "Running array of length " 
+              << array_len 
+              << " (" 
+              << ((array_len*2*sizeof(float))/1e9) 
+              <<"GB)\n";
+    if (validating) {
+        std::cout << "Will validate output\n";
+    }
+    else {
+        std::cout << "Skipping output validation\n";
+    }
+
+    float* input_array;
+    float* output_array;
+    float* validation_array;
+
+    CCC(cudaMallocManaged(&input_array, array_len*sizeof(float)));
+    CCC(cudaMallocManaged(&output_array, array_len*sizeof(float)));
+    if (validating) {
+        CCC(cudaMallocManaged(&validation_array, array_len*sizeof(float)));
+    }
+
+    cudaFree(input_array);
+    cudaFree(output_array);
+    if (validating) {
+        cudaFree(validation_array);
+    }
 }
---
+++ patch/src/shared.cu.h
@@ -0,0 +1,32 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include "cuda.h"
+#include "cuda_runtime.h"
+#include "cuda_runtime_api.h"
+
+// Checking Cuda Call
+#define CCC(call)                                       \
+    {                                                   \
+        cudaError_t cudaStatus = call;                  \
+        if (cudaSuccess != cudaStatus) {                \
+            std::cerr << "ERROR: CUDA RT call \""       \
+                      << #call                          \
+                      << "\" in line "                  \
+                      << __LINE__                       \
+                      << " of file "                    \
+                      << __FILE__                       \
+                      << " failed with "                \
+                      << cudaGetErrorString(cudaStatus) \
+                      << " ("                           \
+                      << cudaStatus                     \
+                      <<").\n",                         \
+            exit(cudaStatus);                           \
+        }                                               \
+    }
+
+#endif
